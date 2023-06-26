083d065a238e355d3688cbee460d6c52d38f9724
unknown <kartik.xworkz@gmail.com>
2023-06-26 19:21:02 +0530

    Variables push

---
+++ Variables.java
@@ -0,0 +1,62 @@
+public class Variables{
+
+	public static void main(String[] args){
+			int totalNumberOfApplication =100;
+			String nameOfTheCourse="Java";
+			String sizeOfTheDisplay="6.7 inches";
+			int numbeOfUnitsSold=25;
+			int numberOfVaccunies=10;
+			int numberOfRedCars=45;
+			int curbWeightOfVehicleInKg=1234;
+			int numberOfRoomsAvailable=69;
+			int totalNumberOfStudents=20;
+			int priceOfTheProduct =100;
+			int capacityOfTheTankInLtrs=1000;
+			String nameOfTheCompany="Hyndai";
+			String hsrpNumber="KA48N3550";
+			String instituteName="XWorkz";
+			String typeOfTheCar="Sedan";
+			String industryType="Automobile";
+			String nameOfTheCar="Verna";
+			String nameOfTheStudent="Kartik";
+			String nameofTheTree="Mango Tree";
+			String typeOfTheRainfall="Frontal Rainfall";
+			String keyboardLayout="QWERTY";
+			String typeOfTheRooms="Luxury";
+			
+			
+			priceOfTheProduct=150;
+			keyboardLayout="QWERTZ";
+			numbeOfUnitsSold=2000;
+			typeOfTheCar="SUV";
+			nameOfTheCompany="Toyota";
+			nameOfTheCourse="Python";
+			numberOfRoomsAvailable=69;
+			priceOfTheProduct=75;
+			typeOfTheRooms="Normal";
+			nameofTheTree="Banyan Tree";
+			
+			System.out.println(totalNumberOfApplication);
+			System.out.println(nameOfTheCourse);
+			System.out.println(sizeOfTheDisplay);
+			System.out.println(numbeOfUnitsSold);
+			System.out.println(numberOfVaccunies);
+			System.out.println(numberOfRedCars);
+			System.out.println(curbWeightOfVehicleInKg);
+			System.out.println(numberOfRoomsAvailable);
+			System.out.println(totalNumberOfStudents);
+			System.out.println(priceOfTheProduct);
+			System.out.println(capacityOfTheTankInLtrs);
+			System.out.println(nameOfTheCompany);
+			System.out.println(hsrpNumber);
+			System.out.println(instituteName);
+			System.out.println(typeOfTheCar);
+			System.out.println(nameOfTheStudent);
+			System.out.println(nameofTheTree);
+			System.out.println(typeOfTheRainfall);
+			System.out.println(keyboardLayout);
+			System.out.println(typeOfTheRooms);
+
+
+	}
+	}
